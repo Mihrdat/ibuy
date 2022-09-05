b5e159374fcd009141440edc77b602ee88e8c6a2
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2022-09-05 15:31:09 +0430

    Create Collection & Product models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Collection(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    inventory = models.PositiveIntegerField()
+    last_update = models.DateField(auto_now=True)
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(0.1)])
+    collection = models.ForeignKey(
+        Collection, on_delete=models.PROTECT, related_name='products')
