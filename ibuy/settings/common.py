64a6dcadfd19c91c31ed7906571c2ac30c86f08b
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2022-09-13 11:50:05 +0430

    Manage media files

--- ibuy/settings/common.py
+++ ibuy/settings/common.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DJANGO_APPS = [
     'django.contrib.admin',
@@ -79,6 +80,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- ibuy/urls.py
+++ ibuy/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 LOCAL_URLS = [
     path('admin/', admin.site.urls),
@@ -14,3 +16,7 @@ THIRD_PARTY_URLS = [
 ]
 
 urlpatterns = LOCAL_URLS + THIRD_PARTY_URLS
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
