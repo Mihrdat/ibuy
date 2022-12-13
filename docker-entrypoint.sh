5613b8f86a90aec68d20fa727a267c95f36b6f78
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2022-12-13 17:26:38 +0330

    Change base image of Dockerfile and make use of multi-stage builds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-echo "Waiting for MySQL to start..."
+echo "+-------------------------------+"
+echo "| Waiting for MySQL to start... |"
+echo "+-------------------------------+"
 ./wait-for-it.sh db:3306
 
-echo "Migrating database..."
+echo "+-----------------------+"
+echo "| Migrating database... |"
+echo "+-----------------------+"
 python manage.py migrate
 
-echo "Starting server..."
+echo "+--------------------+"
+echo "| Starting server... |"
+echo "+--------------------+"
 python manage.py runserver 0.0.0.0:8000
